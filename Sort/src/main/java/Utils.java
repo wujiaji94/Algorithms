2a2083c93a1119f763e178299d4e44711aafe2e4
wujiaji <wujiaji94@163.com>
2021-07-24 17:13:25 +0800

    remove XOR

--- Sort/src/main/java/Utils.java
+++ Sort/src/main/java/Utils.java
@@ -6,8 +6,8 @@
  **/
 public class Utils {
     public static void swap(int[] arr, int i, int j) {
-        arr[i] = arr[i] ^ arr[j];
-        arr[j] = arr[i] ^ arr[j];
-        arr[i] = arr[i] ^ arr[j];
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
